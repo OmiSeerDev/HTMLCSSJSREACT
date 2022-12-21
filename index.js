db3ba54aa39af738a118e2e1a95ed7f08300f351
OmiSeerDev <omarmtinajero@gmail.com>
2022-12-20 20:22:36 -0600

    Component merge

--- index.js
+++ index.js
@@ -40,71 +40,59 @@ cartIcon.addEventListener('click', toggleOrderAside);
 const productList = [];
 productList.push({
     name: "Bike",
-    price: 120,
+    price: 12000.00,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 });
 
 productList.push({
     name: "Purse",
-    price: 13,
+    price: 1300.00,
     image: 'https://cdn.cliqueinc.com/posts/292083/affordable-purse-brands-292083-1661443929747-main.700x0c.jpg'
 });
 
 productList.push({
     name: "Tablet",
-    price: 1250,
+    price: 8550.00,
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQX2v7EDhpcF5jF8cLoY7M39xQ745ABu-lIpg&usqp=CAU'
 });
 
-/*
 
-          <div class="product-card">
-            <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-            <div class="product-info">
-              <div>
-                <p>$120,00</p>
-                <p>Bike</p>
-              </div>
-              <figure>
-                <img src="./icons/bt_add_to_cart.svg" alt="">
-              </figure>
-            </div>
-          </div>
+function renderProducts(arr) {
+    for (product of arr) {
+      const productCard = document.createElement('div');
+      productCard.classList.add('product-card');
+    
+      // product= {name, price, image} -> product.image
+      const productImg = document.createElement('img');
+      productImg.setAttribute('src', product.image);
+    
+      const productInfo = document.createElement('div');
+      productInfo.classList.add('product-info');
+    
+      const productInfoDiv = document.createElement('div');
+    
+      const productPrice = document.createElement('p');
+      productPrice.innerText = '$' + product.price;
+      const productName = document.createElement('p');
+      productName.innerText = product.name;
+    
+      productInfoDiv.appendChild(productPrice);
+      productInfoDiv.appendChild(productName);
+    
+      const productInfoFigure = document.createElement('figure');
+      const productImgCart = document.createElement('img');
+      productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+      productInfoFigure.appendChild(productImgCart);
+    
+      productInfo.appendChild(productInfoDiv);
+      productInfo.appendChild(productInfoFigure);
+    
+      productCard.appendChild(productImg);
+      productCard.appendChild(productInfo);
+    
+      cardsContainer.appendChild(productCard);
+    }
+  }
 
-        </div> */
-for (product of productList) {
-   
-    const productCard = document.createElement('div');
-    productCard.classList.add('product-card');
-  
-    // product= {name, price, image} -> product.image
-    const productImg = document.createElement('img');
-    productImg.setAttribute('src', product.image);
-  
-    const productInfo = document.createElement('div');
-    productInfo.classList.add('product-info');
-  
-    const productInfoDiv = document.createElement('div');
-  
-    const productPrice = document.createElement('p');
-    productPrice.innerText = '$' + product.price;
-    const productName = document.createElement('p');
-    productName.innerText = product.name;
-  
-    productInfoDiv.appendChild(productPrice);
-    productInfoDiv.appendChild(productName);
-  
-    const productInfoFigure = document.createElement('figure');
-    const productImgCart = document.createElement('img');
-    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
-  
-    productInfoFigure.appendChild(productImgCart);
-  
-    productInfo.appendChild(productInfoDiv);
-    productInfo.appendChild(productInfoFigure);
-  
-    productCard.appendChild(productImg);
-    productCard.appendChild(productInfo);
-  
-    cardsContainer.appendChild(productCard);
-}
+renderProducts(productList);
